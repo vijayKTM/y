345d09b330c9d8b4213bb6180e841cb15c7ea014
PUNE-HR-SYSTEM <PUNE-HR-SYSTEM@DESKTOP-NGUUSKQ>
2019-12-30 03:39:32 -0800

    1st commit

---
+++ DemoGit/src/DemoA.java
@@ -0,0 +1,8 @@
+
+public class DemoA {
+	
+	public static void main(String[] args) {
+		System.out.println("hiiiiiii");
+	}
+
+}
---
+++ DemoGit/src/DemoB.java
@@ -0,0 +1,9 @@
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		System.out.println("hellooooo");
+
+	}
+
+}
